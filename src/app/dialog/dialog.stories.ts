83fc390994af8030b8850e0ca5b7d154d9a16918
Royer Adames <royeraadames@gmail.com>
2023-07-24 14:34:12 -0400

    fix: dialog story

--- src/app/dialog/dialog.stories.ts
+++ src/app/dialog/dialog.stories.ts
@@ -1,12 +1,21 @@
-import {moduleMetadata} from '@storybook/angular';
 import {DialogComponent} from "./dialog.component"
+import type { Meta, StoryObj } from '@storybook/angular';
 
-export default {
-  title: 'Components/MyComponent', decorators: [moduleMetadata({
-    declarations: [DialogComponent],
-  }),],
+// More on how to set up stories at: https://storybook.js.org/docs/angular/writing-stories/introduction
+const meta: Meta<DialogComponent> = {
+  title: 'Components/Dialog',
+  component: DialogComponent,
+  tags: ['autodocs'],
+  render: (args: DialogComponent) => ({
+    props: {
+      ...args,
+    },
+  }),
 };
 
-export const Default = () => ({
-  component: DialogComponent, props: {},
-});
+export default meta;
+type Story = StoryObj<DialogComponent>;
+
+// More on writing stories with args: https://storybook.js.org/docs/angular/writing-stories/args
+export const Primary: Story = {
+};
