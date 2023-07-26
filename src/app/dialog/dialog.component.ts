0cd1f6ff5ff616ef464d393ce92391001b6d9f92
Royer Adames <royeraadames@gmail.com>
2023-07-26 02:18:47 -0400

    update: focus on button after close

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,23 +1,51 @@
-import {Component, Input} from '@angular/core';
+import {Component, ElementRef, Input, ViewChild} from '@angular/core';
 
 @Component({
-  selector: 'app-dialog',
-  templateUrl: './dialog.component.html',
-  styleUrls: ['./dialog.component.css']
+  selector: 'app-dialog', templateUrl: './dialog.component.html', styleUrls: ['./dialog.component.css']
 })
 export class DialogComponent {
   @Input() isOpen = false;
+  @ViewChild('open', {static: false}) openButton!: ElementRef;
+  @ViewChild('close', {static: false}) closeButton!: ElementRef;
 
   toggleDialog() {
     this.isOpen = !this.isOpen;
-  }
-  openDialog() {
-    this.isOpen = true;
+    if (this.isOpen) {
+      this.closeButton.nativeElement.focus();
+      this.captureFocus();
+    } else this.openButton.nativeElement.focus();
   }
 
   action(e: KeyboardEvent) {
     if (e.keyCode === 27 || e.key === 'Escape') {
       this.isOpen = false;
     }
+  }
+
+  private captureFocus() {
+    const focusableElements = document.querySelectorAll('a[href], button, textarea, input[type="text"], input[type="radio"], input[type="checkbox"], select');
+    const firstFocusableElement = this.openButton.nativeElement as HTMLElement;
+    const lastFocusableElement = focusableElements[focusableElements.length - 1] as HTMLElement;
+    const KEYCODE_TAB = 9;
+
+    document.addEventListener('keydown', (e: KeyboardEvent) => {
+      const isTabPressed = e.key === 'Tab' || e.keyCode === KEYCODE_TAB;
+
+      if (!isTabPressed) {
+        return;
+      }
+
+      if (e.shiftKey) {
+        if (document.activeElement === firstFocusableElement) {
+          lastFocusableElement.focus();
+          e.preventDefault();
+        }
+      } else {
+        if (document.activeElement === lastFocusableElement) {
+          firstFocusableElement.focus();
+          e.preventDefault();
+        }
+      }
+    });
   }
 }
