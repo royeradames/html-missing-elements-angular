4ec75e0b5d19a7ebca28a779ac7a3c15879d0271
Royer Adames <royeraadames@gmail.com>
2023-07-26 01:27:06 -0400

    update: add modal and template

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-dialog',
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./dialog.component.css']
 })
 export class DialogComponent {
+  @Input() isOpen = false;
 
+  toggleDialog() {
+    this.isOpen = !this.isOpen;
+  }
+  openDialog() {
+    this.isOpen = true;
+  }
+
+  action(e: KeyboardEvent) {
+    if (e.keyCode === 27 || e.key === 'Escape') {
+      this.isOpen = false;
+    }
+  }
 }
