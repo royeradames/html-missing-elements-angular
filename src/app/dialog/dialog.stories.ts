52a2b0c339817e49de46126600493773717073a2
Royer Adames <royeraadames@gmail.com>
2023-08-07 15:39:01 -0400

    update: add stories with button and default behaviors

--- src/app/dialog/dialog.stories.ts
+++ src/app/dialog/dialog.stories.ts
@@ -6,12 +6,22 @@ const meta: Meta<DialogComponent> = {
   title: 'Components/Dialog',
   component: DialogComponent,
   tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<DialogComponent>;
+
+// More on writing stories with args: https://storybook.js.org/docs/angular/writing-stories/args
+export const Default: Story = {
+  args: {
+  },
   render: (args: DialogComponent) => ({
     props: {
       ...args,
+      isOpen: true,
     },
     template: `
-      <app-dialog>
+      <app-dialog *ngIf="isOpen" (onClose)="isOpen = false">
         <div header>Some content</div>
         <div body>
           <p>Some more content</p>
@@ -21,12 +31,47 @@ const meta: Meta<DialogComponent> = {
     `,
   }),
 };
-
-export default meta;
-type Story = StoryObj<DialogComponent>;
-
-// More on writing stories with args: https://storybook.js.org/docs/angular/writing-stories/args
-export const Primary: Story = {
+export const DialogButtonClose: Story = {
   args: {
   },
+  render: (args: DialogComponent) => ({
+    props: {
+      ...args,
+      isOpen: false,
+    },
+    template: `
+      <button #button (click)="isOpen = !isOpen" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Toggle
+      Dialog
+    </button>
+      <app-dialog *ngIf="isOpen" [previousFocusElement]="button" (onClose)="isOpen = false">
+        <div header>Some content</div>
+        <div body>
+          <p>Some more content</p>
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Button that clicks</button>
+        </div>
+      </app-dialog>
+    `,
+  }),
+};
+export const DialogButtonOpen: Story = {
+  args: {
+  },
+  render: (args: DialogComponent) => ({
+    props: {
+      ...args,
+      isOpen: true,
+    },
+    template: `
+      <button #button (click)="isOpen = !isOpen" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Toggle
+      Dialog
+    </button>
+      <app-dialog *ngIf="isOpen" [previousFocusElement]="button" (onClose)="isOpen = false">
+        <div header>Some content</div>
+        <div body>
+          <p>Some more content</p>
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Button that clicks</button>
+        </div>
+      </app-dialog>
+    `,
+  }),
 };
