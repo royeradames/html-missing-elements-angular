322653324bc4e18cbd940685b0933b3abdedb447
Royer Adames <royeraadames@gmail.com>
2023-08-11 19:52:30 -0400

    update: dialog with a keyboard only last button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {DialogComponent} from './dialog/dialog.component';
 import {ButtonComponent} from './button/button.component';
+import {
+  DialogWithLastFousAbleElementComponent
+} from "./dialogWithLastFocusableItem/dialog-with-last-focusable-element.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     DialogComponent,
     ButtonComponent,
+    DialogWithLastFousAbleElementComponent
   ],
   imports: [
     BrowserModule
