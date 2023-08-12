b2f471d6ac12f07312cb0c8cbbf6696baa8dda7b
Royer Adames <royeraadames@gmail.com>
2023-08-12 18:05:13 -0400

    update: move dialogs to their own folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
-import {DialogComponent} from './dialog/dialog.component';
+import {DialogComponent} from './dialogs/dialog/dialog.component';
 import {ButtonComponent} from './button/button.component';
 import {
   DialogWithLastFousAbleElementComponent
-} from "./dialogWithLastFocusableItem/dialog-with-last-focusable-element.component";
-import {Modal} from "./modal/modal.component";
-import {ModernDialogComponent} from "./modernDialog/modern-dialog.component";
-import {ModernDialogWrapComponent} from "./modernDialogWrap/modern-dialog-wrap.component";
+} from "./dialogs/dialogWithLastFocusableItem/dialog-with-last-focusable-element.component";
+import {Modal} from "./dialogs/modal/modal.component";
+import {ModernDialogComponent} from "./dialogs/modernDialog/modern-dialog.component";
+import {ModernDialogWrapComponent} from "./dialogs/modernDialogWrap/modern-dialog-wrap.component";
 
 @NgModule({
   declarations: [
