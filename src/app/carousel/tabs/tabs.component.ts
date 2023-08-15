eb6b1826359447b4719790f3738262502213f076
Royer Adames <royeraadames@gmail.com>
2023-08-15 18:22:08 -0400

    update: center tabs and add extra paddings

--- src/app/carousel/tabs/tabs.component.ts
+++ src/app/carousel/tabs/tabs.component.ts
@@ -15,7 +15,8 @@ interface SlideInterface {
   selector: 'app-tabs', templateUrl: './tabs.component.html', styleUrls: ['./tabs.component.css']
 })
 export class TabsComponent implements OnInit{
-  carouselItems: SlideInterface[] = [{
+  carouselItems: SlideInterface[] = [
+    {
     id: 1,
     href: '#',
     imgSrc: 'https://picsum.photos/id/237/200/300',
@@ -23,7 +24,79 @@ export class TabsComponent implements OnInit{
     captionHref: '#',
     captionTitle: 'Caption title 1',
     captionDescription: 'Caption description 1'
-  }, // ... Add other carousel items here
+  },
+    {
+      id: 2,
+      href: '#',
+      imgSrc: 'https://picsum.photos/id/238/200/300',
+      alt: 'Image description 2',
+      captionHref: '#',
+      captionTitle: 'Caption title 2',
+      captionDescription: 'Caption description 2'
+    },
+    {
+      id: 3,
+      href: '#',
+      imgSrc: 'https://picsum.photos/id/239/200/300',
+      alt: 'Image description 3',
+      captionHref: '#',
+      captionTitle: 'Caption title 3',
+      captionDescription: 'Caption description 3'
+    },
+    {
+      id: 4,
+      href: '#',
+      imgSrc: 'https://picsum.photos/id/240/200/300',
+      alt: 'Image description 4',
+      captionHref: '#',
+      captionTitle: 'Caption title 4',
+      captionDescription: 'Caption description 4'
+    },
+    {
+      id: 5,
+      href: '#',
+      imgSrc: 'https://picsum.photos/id/241/200/300',
+      alt: 'Image description 5',
+      captionHref: '#',
+      captionTitle: 'Caption title 5',
+      captionDescription: 'Caption description 5'
+    },
+    {
+      id: 6,
+      href: '#',
+      imgSrc: 'https://picsum.photos/id/242/200/300',
+      alt: 'Image description 6',
+      captionHref: '#',
+      captionTitle: 'Caption title 6',
+      captionDescription: 'Caption description 6'
+    },
+    {
+      id: 7,
+      href: '#',
+      imgSrc: 'https://picsum.photos/id/243/200/300',
+      alt: 'Image description 7',
+      captionHref: '#',
+      captionTitle: 'Caption title 7',
+      captionDescription: 'Caption description 7'
+    },
+    {
+      id: 8,
+      href: '#',
+      imgSrc: 'https://picsum.photos/id/244/200/300',
+      alt: 'Image description 8',
+      captionHref: '#',
+      captionTitle: 'Caption title 8',
+      captionDescription: 'Caption description 8'
+    },
+    {
+      id: 9,
+      href: '#',
+      imgSrc: 'https://picsum.photos/id/245/200/300',
+      alt: 'Image description 9',
+      captionHref: '#',
+      captionTitle: 'Caption title 9',
+      captionDescription: 'Caption description 9'
+    },
   ];
   isAutoPlayOn?: boolean;
   selectedSlide?: SlideInterface;
